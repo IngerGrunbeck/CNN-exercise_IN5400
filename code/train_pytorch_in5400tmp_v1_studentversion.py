d8d61c719b6415c5e0a49d80da943bb4135a7ba2
Gruenbeck <inger.gruenbeck@gmail.com>
2021-03-20 11:36:50 +0100

    finish up task 1

--- code/train_pytorch_in5400tmp_v1_studentversion.py
+++ code/train_pytorch_in5400tmp_v1_studentversion.py
@@ -16,6 +16,7 @@ from torch import Tensor
 import time
 import os
 import numpy as np
+from statistics import mean
 
 import PIL.Image
 from sklearn.metrics import average_precision_score as AP
@@ -186,17 +187,20 @@ def traineval2_model_nocv(dataloader_train, dataloader_test,  model,  criterion,
         avgperfmeasure = np.mean(perfmeasure)
         print('at epoch: ', epoch, ' avgperfmeasure ', avgperfmeasure)
 
-        if avgperfmeasure > best_measure: #higher is better or lower is better?
+        if avgperfmeasure > best_measure:
             bestweights= model.state_dict()
             best_measure = avgperfmeasure
             best_epoch = epoch
             print('current best', best_measure, ' at epoch ', best_epoch)
+            best_concat_pred = concat_pred
+            best_concat_labels = concat_labels
+            best_fname = fnames
 
-            save_scores(concat_pred, fnames)
+    save_scores(best_concat_pred, best_fname)
+    tailacc(image_scores=best_concat_pred, labels=best_concat_labels, cat_nr=numcl)
+
 
     return best_epoch, best_measure, bestweights, trainlosses, testlosses, testperfs
-
-
 
 
 class BCEWithLogitsLoss(nn.modules.loss._Loss):
@@ -230,26 +234,58 @@ def save_scores(scores, filenames):
         np.save('../saved_scores/' + class_name + '_scores', sorted_scores)
         np.save('../saved_scores/' + class_name + '_filenames', fnames)
 
-def test_train_curve(epochs, labels = 'loss', train_data=None, test_data=None):
+
+def test_train_curve(epochs, labels='loss', train_data=None, test_data=None):
     plt.figure()
-    plt.xticks(np.arange(0, epochs, step=1))
-
-    if train_data:
-        plt.plot(train_data, label='training')
-    if test_data:
-        plt.plot(test_data, label='validation')
-    plt.legend()
-
+    plt.xticks(np.arange(0, epochs, step=2))
     plt.title('Learning Curve')
-    if labels == 'loss':
-        plt.set_ylabel('Loss')
-    else:
-        plt.ylabels('mAP')
-    plt.xlabel('Epochs')
 
+    if labels == 'loss':
+        if train_data:
+            plt.plot(train_data, label='training')
+        if test_data:
+            plt.plot(test_data, label='validation')
+        plt.ylabel('Loss')
+        plt.legend()
+    elif labels == 'map':
+        avg = []
+        for element in test_data:
+            avg.append(np.mean(element))
+        plt.plot(avg)
+        plt.ylabel('mAP')
+
+    plt.xlabel('Epochs')
     plt.show()
 
+def tailacc(image_scores, labels, cat_nr):
+    thresshold = 0.5
+    t_range = 15
+    avg_tailacc = []
+    tailacc_t = [np.empty(shape=(0)) for _ in range(t_range)]
 
+
+    for cat in range(cat_nr):
+        t_max = max(image_scores[cat])
+        stepsize = (t_max - thresshold)/(t_range)
+        t_list = [0.5]
+        for _ in range(t_range - 1):
+            t_list.append(t_list[-1] + stepsize)
+
+
+        binary_scores = [int(x > thresshold) for x in image_scores[cat].tolist()]
+        hard_classifier = [1 if i == j else 0 for i, j in zip(labels[cat].tolist(), binary_scores)]
+        for i, t in enumerate(t_list):
+            soft_classifier = [int(x > t) for x in image_scores[cat].tolist()]
+            tailacc_t[i] = np.append(tailacc_t[i], (1/sum(soft_classifier)*sum([a * b for a, b in zip(hard_classifier, soft_classifier)])))
+
+    for t_avg in tailacc_t:
+        avg_tailacc.append(np.mean(t_avg))
+
+    plt.figure()
+    plt.plot([round(t, 2) for t in t_list], avg_tailacc)
+    plt.title('Average Tailaccuracy')
+    plt.xlabel('t')
+    plt.show()
 
 def runstuff():
     np.random.seed(9400)
@@ -291,8 +327,8 @@ def runstuff():
     image_datasets={}
     image_datasets['train'] = dataset_voc(root_dir='../data/VOCdevkit/VOC2012/',trvaltest=0, transform=data_transforms['train'])
     image_datasets['val'] = dataset_voc(root_dir='../data/VOCdevkit/VOC2012/',trvaltest=1, transform=data_transforms['val'])
-#    image_datasets['train']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=0, transform=data_transforms['train'])
-#    image_datasets['val']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=1, transform=data_transforms['val'])
+    #    image_datasets['train']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=0, transform=data_transforms['train'])
+    #    image_datasets['val']=dataset_voc(root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/VOCdevkit/VOC2012/',trvaltest=1, transform=data_transforms['val'])
 
     #dataloaders
     dataloaders = {}
@@ -329,267 +365,12 @@ def runstuff():
     model.load_state_dict(bestweights)
     torch.save(model, '../saved_model/best_entire_model.pth')
 
-    # plotting the test curves
-    test_train_curve(trainlosses, testlosses)
-    test_train_curve(testperfs)
+    # plotting the learning curves
+    test_train_curve(epochs=config['maxnumepochs'], labels='loss', train_data=trainlosses, test_data=testlosses)
+    test_train_curve(epochs=config['maxnumepochs'], labels='map', test_data=testperfs)
 
 
-
-###########
-# for part2
-###########
-'''
-
-
-def setbyname2(targetmodel,name,value):
-
-    def iteratset(obj,components,value,nametail=[]):
-
-      if not hasattr(obj,components[0]):
-        return False
-      elif len(components)==1:
-        if not hasattr(obj,components[0]):
-          print('object has not the component:',components[0])
-          print('nametail:',nametail)
-          exit()
-        setattr(obj,components[0],value)
-        #print('found!!', components[0])
-        #exit()
-        return True
-      else:
-        nextobj=getattr(obj,components[0])
-        
-        newtail = nametail
-        newtail.append(components[0])
-        #print('components ',components, nametail, newtail)
-        #print(type(obj),type(nextobj))
-
-        return iteratset(nextobj,components[1:],value, nametail= newtail)
-
-    components=name.split('.')
-    success=iteratset(targetmodel,components,value, nametail=[])
-    return success
-    
-    
- 
-class wsconv2(nn.Conv2d):
-  def __init__(self, in_channels, out_channels, kernel_size, stride,
-                     padding, dilation = 1 , groups =1 , bias = None, eps=1e-12 ):
-    super(wsconv2, self).__init__(in_channels, out_channels, kernel_size, stride, padding, dilation, groups, bias)
-
-    self.eps=eps
-
-  def forward(self,x):
-    #torch.nn.functional.conv2d documentation tells about weight shapes
-    pass     
-      
-
-def bntoWSconverter(model):
-
-  #either you modify model in place
-  #or you create a copy of it e.g. using copy.deepcopy(...)  
-  # https://discuss.pytorch.org/t/are-there-any-recommended-methods-to-clone-a-model/483/17
-
-  lastwasconv2= False
-  for nm,module in model.named_modules():
-    #print(nm)
-    
-    if isinstance(module, nn.Conv2d):
-      #replace, get std
-      lastwasconv2= True
-      
-      usedeps= 1e-12 # use 1e-12 if you add it to a variance term, and 1e-6 if you add it to a standard deviation term
-      
-      #TODO 
-      # put in here your wsconv2, dont forget to copy convolution weight and, if exists, the convolution bias into your wsconv2
-      
-      setbyname2(model,nm,newconv)            
-      
-    elif isinstance(module,nn.BatchNorm2d):
-    
-      if False == lastwasconv2:
-        print('got disconnected batchnorm??')
-        exit()
-        
-    
-      print('got one', nm)
-      
-      #TODO 
-      # you will need here data computed from the preceding nn.Conv2d instance which came along your way
-
-      #delete       
-      lastwasconv2= False
-
-    else:
-      lastwasconv2= False
-
-
-
-
-#preprocessing: https://pytorch.org/docs/master/torchvision/models.html
-#transforms: https://pytorch.org/docs/master/torchvision/transforms.html
-#grey images, best dealt before transform
-# at first just smaller side to 224, then 224 random crop or centercrop(224)
-#can do transforms yourself: PIL -> numpy -> your work -> PIL -> ToTensor()
-
-class dataset_imagenetvalpart(Dataset):
-  def __init__(self, root_dir, xmllabeldir, synsetfile, maxnum, transform=None):
-
-    """
-    Args:
-
-        root_dir (string): Directory with all the images.
-        transform (callable, optional): Optional transform to be applied
-            on a sample.
-    """
-
-    self.root_dir = root_dir
-    self.xmllabeldir=xmllabeldir
-    self.transform = transform
-    self.imgfilenames=[]
-    self.labels=[]
-    self.ending=".JPEG"
-
-    self.clsdict=get_classes()
-
-
-    indicestosynsets,self.synsetstoindices,synsetstoclassdescr=parsesynsetwords(synsetfile)
-
-
-    for root, dirs, files in os.walk(self.root_dir):
-       for ct,name in enumerate(files):
-          nm=os.path.join(root, name)
-          #print(nm)
-          if (maxnum >0) and ct>= (maxnum):
-            break
-          self.imgfilenames.append(nm)
-          label,firstname=parseclasslabel(self.filenametoxml(nm) ,self.synsetstoindices)
-          self.labels.append(label)
-
-   
-  def filenametoxml(self,fn):
-    f=os.path.basename(fn)
-     
-    if not f.endswith(self.ending):
-      print('not f.endswith(self.ending)')
-      exit()
-       
-    f=f[:-len(self.ending)]+'.xml'
-    f=os.path.join(self.xmllabeldir,f) 
-     
-    return f
-
-
-  def __len__(self):
-      return len(self.imgfilenames)
-
-  def __getitem__(self, idx):
-    image = PIL.Image.open(self.imgfilenames[idx]).convert('RGB')
-
-    label=self.labels[idx]
-
-    if self.transform:
-      image = self.transform(image)
-
-    #print(image.size())
-
-    sample = {'image': image, 'label': label, 'filename': self.imgfilenames[idx]}
-
-    return sample
-
-
-
- 
-def comparetwomodeloutputs(model1, model2, dataloader, device):
-
-    model1.eval()
-    model2.eval()
-
-    curcount = 0
-    avgdiff = 0
-    
-    with torch.no_grad():
-      for batch_idx, data in enumerate(dataloader):
-      
-      
-          if (batch_idx%100==0) and (batch_idx>=100):
-              print('at val batchindex: ', batch_idx)
-      
-          inputs = data['image'].to(device)        
-          outputs1 = model1(inputs)
-          outputs2 = model2(inputs)
-
-          diff=torch.mean(torch.abs((outputs1-outputs2).flatten())) 
-          
-          labels = data['label']
-          print('diff',diff.item())
-          avgdiff = avgdiff*( curcount/ float(curcount+labels.shape[0]) ) + diff.item()* ( labels.shape[0]/ float(curcount+labels.shape[0]) )
-          
-
-          curcount+= labels.shape[0]
-
-    return avgdiff
-
-  
-#routine to test that your copied model at evaluation time works as intended
-def test_WSconversion():
-  
-
-  config = dict()
-  
-  #config['use_gpu'] = True
-  #config['lr']=0.008 #0.005
-  #config['batchsize_train'] = 2
-  #config['batchsize_val'] = 64
-  
-  #data augmentations
-  data_transforms = {
-      'val': transforms.Compose([
-          transforms.Resize(224),
-          transforms.CenterCrop(224),
-          #transforms.RandomHorizontalFlip(), # we want no randomness here :)
-          transforms.ToTensor(),
-          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-      ]),
-  }
-  
-  root_dir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/imagenet300/'
-  xmllabeldir='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/val/'
-  synsetfile='/itf-fi-ml/shared/IN5400/dataforall/mandatory1/students/synset_words.txt'
-
-  dset= dataset_imagenetvalpart(root_dir, xmllabeldir, synsetfile, maxnum=64, transform=data_transforms['val'])
-  dataloader =  torch.utils.data.DataLoader(dset, batch_size=64, shuffle=False) #, num_workers=1) 
-
-  import copy
-  device=torch.device('cpu')  
-  #model
-  model = models.resnet18(pretrained=True)
-  model2 = copy.deepcopy(model.to('cpu'))
-  
-  ####################
-  # assumes it changes the model in-place, use model2= bntoWSconverter(model) if your routine instead modifies a copy of model and returns it
-  ######################
-  bntoWSconverter(model2)
-  
-  model = model.to(device)  
-  model2 = model2.to(device)  
-  
-  avgdiff = comparetwomodeloutputs(model, model2, dataloader, device)
-  
-
-  
-  print('model checking averaged difference', avgdiff )  # order 1e-3 is okay, 1e-2 is still okay.
-  
-    
-    
-    
-'''
-    
-
-
-
-if __name__=='__main__':
-
-  runstuff()
+if __name__ == '__main__':
+    runstuff()
 
 
